259aeca79c6b6decbf29be33c7554e5c6937bd38
Anirudh <sanjeev196945@gmail.com>
2019-12-08 01:06:48 +0530

    User Profile Page

--- frontend/src/app/components/students/signup2/signup2.component.ts
+++ frontend/src/app/components/students/signup2/signup2.component.ts
@@ -2,7 +2,7 @@ import { AuthService } from 'src/app/auth/auth.service';
 import { InterestSendingService } from "./../../../shared/interests/interest-sending.service";
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from '@angular/forms';
-
+export var bio
 @Component({
   selector: "app-signup2",
   templateUrl: "./signup2.component.html",
@@ -24,6 +24,8 @@ export class Signup2Component implements OnInit {
     this.auth.logout()
   }
   onSubmit(form: NgForm) {
+  bio=(form.value['bio'])
+  console.log(bio)
     this.interestsendingservice.postInterest(form.value).subscribe(
       res => {
         console.log("Auth is successful");
--- frontend/src/app/userprofile/userprofile.component.ts
+++ frontend/src/app/userprofile/userprofile.component.ts
@@ -1,10 +1,10 @@
+import { bio } from './../components/students/signup2/signup2.component';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
 import { Achievements } from '../shared/achievements/achievements.model';
 import { AchievementsService } from '../shared/achievements/achievements.service';
-
 @Component({
   selector: 'app-userprofile',
   templateUrl: './userprofile.component.html',
@@ -12,12 +12,14 @@ import { AchievementsService } from '../shared/achievements/achievements.service
 })
 export class UserprofileComponent implements OnInit {
   ach_list: any;
+  bio=bio
 
   constructor( private auth: AuthService,
     private router: Router,private http:HttpClient,public achService: AchievementsService) { }
 
   ngOnInit() {
     this.getAch()
+    console.log(bio)
   }
   logout() {
     this.auth.logout();

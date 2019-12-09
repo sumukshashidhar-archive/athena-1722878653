03d92a8671f38efddc2768cc36cec9dfa0ac1d16
Anirudh <sanjeev196945@gmail.com>
2019-12-09 11:10:49 +0530

    Good bro

--- frontend/src/app/userprofile/userprofile.component.ts
+++ frontend/src/app/userprofile/userprofile.component.ts
@@ -1,3 +1,4 @@
+
 import { bio } from './../components/students/signup2/signup2.component';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
@@ -5,6 +6,8 @@ import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
 import { Achievements } from '../shared/achievements/achievements.model';
 import { AchievementsService } from '../shared/achievements/achievements.service';
+import * as jwt_decode from 'jwt-decode';
+
 @Component({
   selector: 'app-userprofile',
   templateUrl: './userprofile.component.html',
@@ -12,12 +15,20 @@ import { AchievementsService } from '../shared/achievements/achievements.service
 })
 export class UserprofileComponent implements OnInit {
   ach_list: any;
+   decoded:any
   bio=bio
-
+  username:any
   constructor( private auth: AuthService,
-    private router: Router,private http:HttpClient,public achService: AchievementsService) { }
+    private router: Router,private http:HttpClient,public achService: AchievementsService) {
+      this.decoded = localStorage.getItem('access_token');
+     }
 
   ngOnInit() {
+    var decodedtoken= jwt_decode(this.decoded)
+    if (decodedtoken["role"] == "Student") {
+      this.username = decodedtoken["given_name"];
+    }
+    console.log(this.decoded)
     this.getAch()
     console.log(bio)
   }

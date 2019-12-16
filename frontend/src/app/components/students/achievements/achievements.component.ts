94c54f9e6595cc449dc7f2612a312be50fbc530a
Anirudh <sanjeev196945@gmail.com>
2019-12-16 22:14:25 +0530

    File path retrieve for achievements

--- frontend/src/app/components/students/achievements/achievements.component.ts
+++ frontend/src/app/components/students/achievements/achievements.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from 'src/app/auth/auth.service';
 import { Component, OnInit } from "@angular/core";
 
 import { AchievementsService } from "./../../../shared/achievements/achievements.service";
@@ -17,8 +18,10 @@ export class AchievementsComponent implements OnInit {
   uploadedFiles: Array < File > ;
   showSpinner: boolean = true;
   ach_list: any;
+   link:string
+   actualLink:any
 
-  constructor(public achService: AchievementsService, private router: Router,private http: HttpClient) {}
+  constructor(public achService: AchievementsService, private router: Router,private http: HttpClient, public auth:AuthService)  {}
 
   ngOnInit() {
     this.refreshAchievements();
@@ -51,6 +54,10 @@ export class AchievementsComponent implements OnInit {
     );
   }
 
+
+    logout(){
+      this.auth.logout()
+    }
   refreshAchievements() {
     this.achService.getAchievements().subscribe(res => {
       this.ach_list = res as Achievements[];

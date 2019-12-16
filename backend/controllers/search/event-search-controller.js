3bade153453e9dab085fd0aa15151e7a4962c8f7
sumukshashidhar <sumukuuu@gmail.com>
2019-12-16 12:00:00 +0530

    Event Search Functionality Update 3

--- backend/controllers/search/event-search-controller.js
+++ backend/controllers/search/event-search-controller.js
@@ -3,16 +3,16 @@ var event = require('./../../models/event')
 
 module.exports = {
     regSearch: function regSearch(keyword) {
-        event.find({}, function(err, obj){
+        event.find({}, function(err, RETURNED_EVENTS){
             if(err) {
 
             }
             else {
-                if(obj) {
-
+                if(RETURNED_EVENTS) {
+                    console.log(RETURNED_EVENTS)
                 }
                 else {
-                    
+
                 }
             }
         })

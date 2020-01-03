336c7051455c4626f8a3245564ff010a4aa143c8
sumukshashidhar <sumukuuu@gmail.com>
2020-01-03 08:47:35 +0000

    Reverted

---
+++ backend/microservices/davinci-micro.js
@@ -0,0 +1,20 @@
+var sr = require('./evn-micro')
+
+
+
+
+module.exports = {
+    explore: async function(student) {
+        //Have to return all events in that particular city
+        //So here
+        
+    }, 
+    testexplore: async function(student) {
+        var callback = new Promise((res, rej) => {
+            
+        })
+
+        let r = await callback; 
+        return r;
+    }, 
+}

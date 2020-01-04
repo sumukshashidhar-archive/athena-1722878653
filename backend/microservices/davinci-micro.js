6d7f2ab06f3f57bc26ef09e6f3004197d8c0a221
sumukshashidhar <sumukuuu@gmail.com>
2020-01-04 23:06:11 +0530

    Added a way for people to try both recommended and non recommended versions of events

--- backend/microservices/davinci-micro.js
+++ backend/microservices/davinci-micro.js
@@ -39,4 +39,17 @@ module.exports = {
         let r = await callback; 
         return r;
     }, 
+
+    testexplore2: async function(student) {
+        var callback = new Promise(async (res, rej) => {
+            var PRCSEvns = await sr.all()
+            //These are the events to process
+            //Here comes the recommendations
+            res(PRCSEvns)
+            
+        })
+
+        let r = await callback; 
+        return r;
+    },
 }

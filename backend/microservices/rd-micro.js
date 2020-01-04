f2fa83ae4aa0dd4d3c93c245a07a16588f79be26
sumukshashidhar <sumukuuu@gmail.com>
2020-01-04 23:15:26 +0530

    Added binary search for events rd

--- backend/microservices/rd-micro.js
+++ backend/microservices/rd-micro.js
@@ -104,6 +104,28 @@ async function rdv2(decodedToken, evns) {
     return r
 }
 
+function binarySearch(arr, x, start, end) { 
+        
+    // Base Condtion 
+    if (start > end) return false; 
+
+    // Find the middle index 
+    let mid=Math.floor((start + end)/2); 
+
+    // Compare mid with given key x 
+    if (arr[mid]===x) return true; 
+        
+    // If element at mid is greater than x, 
+    // search in the left half of mid 
+    if(arr[mid] > x)  
+        return binarySearch(arr, x, start, mid-1); 
+    else
+
+        // If element at mid is smaller than x, 
+        // search in the right half of mid 
+        return binarySearch(arr, x, mid+1, end); 
+}
+
 function GetSortOrder(prop) {
     return function(a, b) {
         if(a[prop]  < b[prop]) {

2f76148fbb59fed57b7b0535c00f7f727f898413
sumukshashidhar <sumukuuu@gmail.com>
2020-01-06 14:20:48 +0530

    Added a few search functions

--- backend/microservices/evn-micro.js
+++ backend/microservices/evn-micro.js
@@ -31,10 +31,47 @@ module.exports = {
                 }
             })
         })
-
         let r = await callback;
         return r
-
-    }
+    }, 
+    search_normal: async function() {
+        var callback = new Promise((res, rej)=> {
+            event.find({}, async function(err, OBJ) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    res(OBJ)
+                }
+            })
+            let r = await callback;
+            return r
+        })
+    },
+    search_deep: async function() {
+        var callback = new Promise((res, rej)=> {
+            event.find({}, async function(err, OBJ) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    res(OBJ)
+                }
+            })
+            let r = await callback;
+            return r
+        })
+    }, 
+    search_archive: async function() {
+        var callback = new Promise((res, rej)=> {
+            event.find({}, async function(err, OBJ) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    res(OBJ)
+                }
+            })
+            let r = await callback;
+            return r
+        })
+    }, 
 }
 

194595f3fba7eb95e9b2a5fff42c60d9f5fe25cc
sumukshashidhar <sumukuuu@gmail.com>
2020-01-11 12:32:27 +0530

    Working Events Archive

--- backend/app.js
+++ backend/app.js
@@ -1850,4 +1850,72 @@ app.post('/api/search/users', async function(req, res) {
 
 app.get('/api/run', function(req, res) {
     ///Archiving Events
+    var key = ''
+    eventsArchive()
+
 })
+function eventsArchive() {
+    var events_to_delete;
+    event.find({}, function(err, EVN_OBJECT) {
+        if(err) {
+            console.log(err)
+        }
+        else {
+            if(EVN_OBJECT) {
+                console.log(EVN_OBJECT)
+                console.log("Got the object")
+                var total_length = EVN_OBJECT.length
+                for(let i=0; i<total_length; i++) {
+                    var CUR_EVENT = EVN_OBJECT[i]
+                    console.log('Archiving event at: ', i, 'of ', total_length)
+                    if(CUR_EVENT["evnEndDate"] < Date.now()) {
+                        var newArchEvent = new archevent({
+                            evnName: cur.evnName, 
+                            evnDate1: cur.evnDate1,
+                            evnDate2: cur.evnDate2,
+                            evnInterests: cur.evnInterests, 
+                            evnOrganizerName: cur.evnOrganizerName,
+                            evnOrganizerPage: cur.evnOrganizerPage,
+                            evnOrganizerContact: cur.evnOrganizerContact,
+                            evnLocation: cur.evnLocation, 
+                            evnPincode: cur.evnPincode,
+                            evnAddress: cur.evnAddress, 
+                            evnTargetAge: cur.evnTargetAge,
+                            // CONTACT SUMUK BELOW THIS
+                            evnDescription: cur.evnDescription, 
+                            evnRating: cur.evnRating,
+                            evnAttendees: cur.evnAttendees,
+                            Image: cur.Image
+                        })
+                        newArchEvent.save(function(err, obj) {
+                            if(err) {
+                                console.log('INTERNAL ERROR. FAILED TO PUSH TO MONGO');
+                            }
+                            else {
+                                console.log('SUCCESS >>> PUSHED TO ARCHIVED');
+                                console.log(obj)
+                                events_to_delete.push(cur._id)
+                            }
+                        })
+                    }
+                    
+                }
+                total_length = events_to_delete.length
+                for(let j=0; j < total_length; j++) {
+                    event.deleteOne({_id:events_to_delete[j]}, function(err, DELETED) {
+                        if(err) {
+                            console.log(err)
+                        }
+                        else {
+                            console.log("Deleted")
+                        }
+                    })
+                }
+                console.log("Deleted all redundant achievements")
+            }
+            else {
+                console.log("got blank from mongo")
+            }
+        }
+    })
+}

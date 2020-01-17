3a4919daf44e9e9a0a0bb78b67eaf807d78ae1da
Sumuk Shashidhar <sumukuuu@gmail.com>
2020-01-17 09:32:05 +0530

    Update rd-micro.js

--- backend/microservices/rd-micro.js
+++ backend/microservices/rd-micro.js
@@ -7,89 +7,43 @@ async function rd(decodedToken, evns) {
         var total_length = evns.length
         var sum_array=[];
         var stuobj = await sms.getStudentForRd(decodedToken.usrid)
-        for(let i=0; i<total_length; i++) {
-            var ev = evns[i] //To sesdlect an event
-            var sum = 0.0
-            console.log("THE TARGET AGE IS;", ev.evnTargetAge)
-            console.log("REACHED: OUTER LOOP")
-            if (decodedToken.pincode!=undefined && ev.evnPincode!=undefined) {
-                sum -= (Math.abs(decodedToken.pincode - ev.evnPincode)*0.7) //0.7 because the pincode should not be given that much weightage
-                console.log((Math.abs(decodedToken.pincode - ev.evnPincode)*0.7))
-                console.log('Sum at step 1: ', sum)
-                console.log("REACHED: COMPARISON STEP FOR PINCODE")
+        if(stuobj.response) {
+            var stu = stuobj.obj;
+            for(let i=0; i<total_length; i++) {
+                var ev = evns[i] //To sesdlect an event
+                var sum = 0.0
+                console.log("THE TARGET AGE IS;", ev.evnTargetAge)
+                console.log("REACHED: OUTER LOOP")
+                if (stu.pincode!=undefined && ev.evnPincode!=undefined) {
+                    sum -= (Math.abs(stu.pincode - ev.evnPincode)*0.7) //0.7 because the pincode should not be given that much weightage
+                    console.log('Sum at step 1: ', sum)
+                    console.log("REACHED: COMPARISON STEP FOR PINCODE")
+                }
+                if (ev.evnTargetAge!=undefined && stu.Age!=undefined) {
+                    //Subtracting Raw Target Age Difference
+                    sum -= (Math.abs(decodedToken.age - ev.evnTargetAge)*0.5)
+                    console.log((Math.abs(decodedToken.age - ev.evnTargetAge)*0.5))
+                    console.log('Sum at step 2: ', sum)
+                }
+                var n = 1
+                
+                //Step 3 starts here
+
+
+
+
+
+
+                //Final Code for pushing the sum to the sum array
+                sum_array.push(sum)
+                evns[i].evnScore = sum;
+                
             }
-            if (ev.evnTargetAge!=undefined && decodedToken.age!=undefined) {
-                //Subtracting Raw Target Age Difference
-                sum -= (Math.abs(decodedToken.age - ev.evnTargetAge)*0.5)
-                console.log((Math.abs(decodedToken.age - ev.evnTargetAge)*0.5))
-                console.log('Sum at step 2: ', sum)
-            }
-            var n = 1            
-            //This is the interests
-            // Student.findOne({_id: decodedToken.usrid}, function(err, obj) {
-            //     if(err) {
-            //         console.log(err)
-            //     }
-            //     else {
-            //         console.log('Now got the student. Will continue with the recommendation')
-            //     }
-            // })
-            // for(let m=0; m < decodedToken.interests.length; m++) {
-            //     if(ev.evnInterests.includes(decodedToken.interests[m])) {
-            //         sum = sum + sum*n;
-            //         n = n+1;
-            //     }
-            //     else {
-                    
-            //     }
-            // }
-
-            //BASIC RECOMMENDATIONS
-    
-            // => Further comes the events searchin
-            // var n = 1;
-            // var event_interests = ev.evnInterests;
-            // var userinterests = decodedToken.interests;
-            // var tot_event_interests = ev.evnInterests.length;
-
-            // //
-            // for(let j=0; j<userinterests.length; j++) {
-            //     console.log('comparing interests')
-            //     if(event_interests.includes(userinterests[j])) {
-            //         sum = sum + n*1
-            //         n++
-            //     }
-            //     else {
-
-            //     }
-            // }
-            // var vectors = decodedToken.uservector;
-            // for(let m=0; m< vectors.length; m++) {
-            //     console.log('comparing interests')
-            //     if(event_interests.includes(userinterests[j])) {
-            //         sum = sum + n*1
-            //         n++
-            //     }
-            //     else {
-                    
-            //     }
-            // }
-            // for(let j=0; j < tot_event_interests; j++) {
-            //     // console.log('Entering loop ' , j)
-            //     // console.log("REACHED: INNER EVENT COMPARISON LOOP")
-            //     for(let k=0; k < userinterests.length; k++) { //have to change this to binary search
-            //         if(event_interests[j]==userinterests[k]) {
-            //             sum +=10
-            //         }
-            //     }
-            // }
-            // console.log("REACHED: OUTSIDE, REACHED FINAL LOOP")
-            sum_array.push(sum)
-            evns[i].evnScore = sum;
-            // console.log(evns)
-            // console.log(decodedToken)
-            
         }
+        else {
+            console.log('The RD Failed. Returning the Events as is')
+        }
+
         res(evns)
     })
 

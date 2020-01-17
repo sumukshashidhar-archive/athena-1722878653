3013172408bd51faabffce5a0ca66ceaf0a50d02
Sumuk Shashidhar <sumukuuu@gmail.com>
2020-01-17 09:44:31 +0530

    Update student-micro.js

--- backend/microservices/student-micro.js
+++ backend/microservices/student-micro.js
@@ -6,7 +6,7 @@ var Student = require('./../models/StudentInfo')
 
 module.exports = {
     getStudentForRd: async function getStudent(id) {
-        var callback = new Promise((res, rej) => {
+        var callback = new Promise(async (res, rej) => {
             Student.findOne({_id: id}, {Interests: 1, Age: 1, pincode: 1, uservector: 1},  async function(err, obj) {
                 if(err) {
                     console.log(err)
@@ -38,7 +38,7 @@ module.exports = {
 
     }, 
     getStudentHeavybyID: async function getStudent(id) {
-        var callback = new Promise((res, rej) => {
+        var callback = new Promise(async (res, rej) => {
             Student.findOne({_id: id},  async function(err, obj) {
                 if(err) {
                     console.log(err)
@@ -70,7 +70,7 @@ module.exports = {
 
     }, 
     getStudentHeavybyID: async function getStudent(email) {
-        var callback = new Promise((res, rej) => {
+        var callback = new Promise(async (res, rej) => {
             Student.findOne({EmailId: email},  async function(err, obj) {
                 if(err) {
                     console.log(err)

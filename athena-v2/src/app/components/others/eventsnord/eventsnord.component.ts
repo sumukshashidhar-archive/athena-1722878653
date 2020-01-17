e88fdcdf59859c5a1905e85d42e72a8cfe3f54c6
Anand Rajaram <anandrajaram21@gmail.com>
2020-01-17 14:12:46 +0530

    fixed date

--- athena-v2/src/app/components/others/eventsnord/eventsnord.component.ts
+++ athena-v2/src/app/components/others/eventsnord/eventsnord.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { EventService } from './../../../shared/events/event.service';
 
 @Component({
   selector: 'app-eventsnord',
@@ -9,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
 export class EventsnordComponent implements OnInit {
   events: any
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, public eventService: EventService) { }
 
   ngOnInit() {
     this.refreshEvents();
@@ -18,7 +19,8 @@ export class EventsnordComponent implements OnInit {
   refreshEvents(){
     this.http.get("http://localhost:3000/eventsdeep").subscribe(
       res => {
-        this.events = res
+        var x = this.eventService.changeDate(res)
+        this.events = x
         console.log(res)
       },
       err => {

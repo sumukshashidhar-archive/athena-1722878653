a040c445467f8ff5e53a02b15b639382503478ce
Sumuk Shashidhar <sumukuuu@gmail.com>
2020-01-18 08:26:42 +0000

    Made evns light

--- backend/microservices/evn-micro.js
+++ backend/microservices/evn-micro.js
@@ -7,7 +7,7 @@ module.exports = {
     cityspecific: async function (city) {
         console.log(city)
         var callback = new Promise((res, rej) => {
-            event.find({ evnCity: city }, async function (err, OBJ) {
+            event.find({ evnCity: city },{evnName: 1, evnDate1: 1, evnDate2: 1, evnInterests: 1, Image:1, evnLocation: 1, evnCity: 1, evnTargetAge: 1}, async function (err, OBJ) {
                 if (err) {
                     console.log(err)
                 }

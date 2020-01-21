432d2ca5349a77c76167bd9a6a425b1000c7f9bc
Anand Rajaram <anandrajaram21@gmail.com>
2020-01-21 17:24:50 +0530

    Events organizer made proper

--- backend/app.js
+++ backend/app.js
@@ -1808,7 +1808,7 @@ async function evnFind(idx) {
 
 async function evnFindLite(idx) {
     var callback = new Promise(function (res, rej) {
-        event.findOne({ _id: idx }, { evnName: 1, Image: 1, evnDate: 1, evnDescription: 1, _id: 1 }, function (err, obj) {
+        event.findOne({ _id: idx }, { evnName: 1, Image: 1, evnDate1: 1,evnDate2: 1, evnDescription: 1, _id: 1 }, function (err, obj) {
             if (err) {
                 console.log(err)
             } else {

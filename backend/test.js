42d4517276e5554ab1624a7c08eb97c0dbacc937
Sumuk Shashidhar <sumukuuu@gmail.com>
2020-01-25 23:39:20 +0530

    WORKING DOCKER + REMOVAL OF REDUNDANT FILES

--- backend/test.js
+++
@@ -1,3 +0,0 @@
-const nanoid = require("nanoid");
-const id = nanoid(100);
-console.log(id)
--- backend/tests.js
+++
@@ -1,5 +0,0 @@
-var d = new Date().getDay().toString();
-var m = new Date().getMonth().toString();
-var y = new Date().getFullYear().toString();
-var thro = new Date().toISOString()
-console.log(d + m + y + thro.slice(0, 11))

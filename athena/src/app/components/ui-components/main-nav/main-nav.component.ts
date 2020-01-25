5aeec9d57c661f9106f52f21b2829f84940e83bc
Anand Rajaram <anandrajaram21@gmail.com>
2020-01-25 19:04:15 +0530

    FIXED ALL ERRORS AND DEPLOYED

--- athena/src/app/components/ui-components/main-nav/main-nav.component.ts
+++ athena/src/app/components/ui-components/main-nav/main-nav.component.ts
@@ -3,8 +3,11 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { AuthService } from './../../../shared/auth/auth.service';
+import { SearchService } from './../../../shared/search/search.service'
 import * as jwt_decode from "jwt-decode";
+import { EventService } from './../../../shared/events/event.service'
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-main-nav',
@@ -27,7 +30,7 @@ export class MainNavComponent {
       this.router.navigate(["/login"]);
     }
 
-  constructor(private breakpointObserver: BreakpointObserver, private auth: AuthService, private router: Router) {
+  constructor(private breakpointObserver: BreakpointObserver, private auth: AuthService, private router: Router, public searchService: SearchService, public eventService: EventService) {
     var decoded = localStorage.getItem("access_token");
     var decodedtoken = jwt_decode(decoded);
 
@@ -39,6 +42,36 @@ export class MainNavComponent {
       this.isOrg = true;
       this.username = decodedtoken["name"];
     }
+  }
+
+  search(form: NgForm) {
+    form.value["usecase"] = 1;
+    console.log(form.value);
+    this.searchService.postSearch(form.value).subscribe(
+      res => {
+        this.searchService.results = null;
+        this.searchService.userResults = null;
+        this.searchService.interestResults = null;
+        this.searchService.orgResults = null;
+        var x = this.eventService.changeDate(res);
+        this.searchService.results = x;
+        console.log(res);
+        this.searchService.tabChange = 0;
+        if (this.searchService.results.length === 0) {
+          this.searchService.message = "Sorry, no results found";
+        } else {
+          this.searchService.message = "We found these results";
+        }
+        this.router.navigate(["/searchres"]);
+      },
+      err => {
+        if (err.status === 422) {
+          console.log(422);
+        } else {
+          console.log(err);
+        }
+      }
+    );
   }
 
 }

073d00b9cfbeb80c243225950e2568366db23f74
Anand Rajaram <anandrajaram21@gmail.com>
2020-01-27 11:37:50 +0530

    fixed backend

--- backend/microservices/evn-micro.js
+++ backend/microservices/evn-micro.js
@@ -7,7 +7,7 @@ module.exports = {
     cityspecific: async function (city) {
         console.log(city)
         var callback = new Promise((res, rej) => {
-            event.find({ evnCity: city },{evnDescription: 1, evnName: 1, evnDate1: 1, evnDate2: 1, evnInterests: 1, Image:1, evnLocation: 1, evnCity: 1, evnTargetAge: 1}, async function (err, OBJ) {
+            event.find({ evnCity: city },{evnPincode: 1, evnDescription: 1, evnName: 1, evnDate1: 1, evnDate2: 1, evnInterests: 1, Image:1, evnLocation: 1, evnCity: 1, evnTargetAge: 1}, async function (err, OBJ) {
                 if (err) {
                     console.log(err)
                 }
@@ -24,7 +24,7 @@ module.exports = {
     },
     all: async function () {
         var callback = new Promise((res, rej) => {
-            event.find({}, {evnDescription: 1, evnName: 1, evnDate1: 1, evnDate2: 1, evnInterests: 1, Image:1, evnLocation: 1, evnCity: 1, evnTargetAge: 1},async function (err, OBJ) {
+            event.find({}, {evnPincode: 1, evnDescription: 1, evnName: 1, evnDate1: 1, evnDate2: 1, evnInterests: 1, Image:1, evnLocation: 1, evnCity: 1, evnTargetAge: 1},async function (err, OBJ) {
                 if (err) {
                     console.log(err)
                 }
